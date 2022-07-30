1531c3df9f5b6cd5cf44ca2e9b88e8d30c5e2b39
Kyltism <vkyrsaxvalyt@mail.ru>
2022-07-30 13:18:49 +0300

    frgfrefre

--- Katya/my-app/src/Components/Footer.js
+++ Katya/my-app/src/Components/Footer.js
@@ -4,8 +4,10 @@ export default function Footer() {
     return (
         <footer>
             <hr></hr>
+            <div className='footer'>
             <div> © {new Date().getFullYear()} BOOKSTORE</div>
             <div>All rights reserved</div>
+            </div>
         </footer >
     )
 }
