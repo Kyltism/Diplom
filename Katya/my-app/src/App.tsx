ac0cfd62fe0c0856f24d179c69c0730be4a76294
katyalevanova <jorikby@mail.ru>
2022-07-30 13:24:51 +0300

    dqd

--- Katya/my-app/src/App.tsx
+++ Katya/my-app/src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Subscribe from './Components/Subscribe';
+import Books from './Components/books';
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
+      <Books/>
       <Subscribe/>
       <Footer />
     </div>
