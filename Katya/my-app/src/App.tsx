0d00464d401a0f5ba5522b87052c7168a7165fac
Kyltism <68355293+Kyltism@users.noreply.github.com>
2022-07-30 15:07:25 +0300

    dfgh

--- Katya/my-app/src/App.tsx
+++ Katya/my-app/src/App.tsx
@@ -4,15 +4,32 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Subscribe from './Components/Subscribe';
 import Books from './Components/books/Books';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <Books/>
-      <Subscribe/>
-      <Footer />
-    </div>
+    <>
+      <div className="wrapper">
+        <Header />
+        <Books />
+        <Subscribe />
+        <Footer />
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+
+            
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
+    </>
   );
 }
 
