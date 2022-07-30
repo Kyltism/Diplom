3b177ae5e859b618cd9ca90039fbecf705328d86
Kyltism <vkyrsaxvalyt@mail.ru>
2022-07-30 13:25:50 +0300

    fgh

--- Katya/my-app/src/App.tsx
+++ Katya/my-app/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Subscribe from './Components/Subscribe';
-import Books from './Components/books';
+import Books from './Components/Books';
 
 function App() {
   return (
